76a19b180c058eb537543da9d34526e5a6db3d6c
Judson Paiva <judson.paiva@hotmail.com>
2026-02-27 19:28:22 +0100

    conhecendo useState

--- first_project/src/App.tsx
+++ first_project/src/App.tsx
@@ -1,15 +1,38 @@
-import { Header } from './component/header';
-import { Aluno } from './component/aluno';
-import { Footer } from './component/footer';
+import { useState } from 'react';
 
 
 export default function App() {
+
+  const [input, setInput] = useState("");
+  const [aluno, setAluno] = useState("");
+  const [idade, setIdade] = useState("");
+
+  function mostrarAluno(){
+    console.log(input);
+    setAluno(input);
+  }
+
   return (
     <div>
-      <Header title="Cabecalho Aprendido" />
-      <Aluno nome="Judson Paiva" idade={15} />
-      <Aluno nome="Leonor Paiva" idade={10} />
-      <Footer />
+      <h1>Conhecendo Use State</h1>
+
+      <input type="text" 
+          placeholder="Digite seu nome" 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)} 
+      />
+
+      <br />
+      <input type="number" 
+          placeholder="Digite sua idade" 
+          value={idade} 
+          onChange={(e) => setIdade(e.target.value)} 
+      />
+
+      <br />
+      <button onClick={mostrarAluno}>Mostrar Aluno</button>
+      <h2>Bem-vindo(a) Aluno(a): {aluno}</h2>
+      <h2>Idade: {idade}</h2>
     </div>
   )
 }
